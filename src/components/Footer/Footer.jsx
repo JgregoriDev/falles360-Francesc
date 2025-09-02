9f64c6af929cdb1fd696581146b7cd6e5ed907f9
“Your <you@example.com>
2025-09-02 13:19:17 +0200

    Footer 1a versió

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,26 @@
+import {Logo} from "@components/Logo/Logo";
+import "./Footer.css";
 export const Footer = () => {
     return (
-        <footer>Footer</footer>
+        // TODO: Acabar Footer
+        <footer className="footer">
+            <div className="container__footer">
+                <div className="footer__logo">
+                    <Logo />
+                </div>
+                <div className="footer__links">
+                    <a href="/protecció">Protecció de dades.</a>
+                </div>
+                
+            </div>
+            <div className="footer__social-network">
+                <a href="https://facebook.es" target="" title="Facebook"></a>
+                <a href="x.com" title="X"></a>
+                <a href="instagram" title="Instagram"></a>
+            </div>
+            {/* Redes Sociales */}
+            {/* Menu */}
+        </footer>
     )
 }
 
