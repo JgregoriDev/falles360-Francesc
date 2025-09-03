7550e1572892d1bd30c6ee3ffbbe90a308d4fd3c
“Your <you@example.com>
2025-09-03 19:43:49 +0200

    Correccion coin css y coin jsx

--- src/components/Coins/Coins.jsx
+++ src/components/Coins/Coins.jsx
@@ -1,18 +1,50 @@
-import './Coins.css';
-import user1 from '@/assets/user1.webp';
-import user2 from '@/assets/user2.webp';
-import user3 from '@/assets/user3.webp';
-import user4 from '@/assets/user4.webp';
-import user5 from '@/assets/user5.webp';
+import "./Coins.css";
+import user1 from "@/assets/user1.webp";
+import user2 from "@/assets/user2.webp";
+import user3 from "@/assets/user3.webp";
+import user4 from "@/assets/user4.webp";
+import user5 from "@/assets/user5.webp";
 export const Coins = () => {
-    return (
-        <div className='Coins__list'>
-                <img src={`${user1}`} alt="user1" className="Coins__image" loading='lazy' />
-                <img src={`${user2}`} alt="user2" className="Coins__image" loading='lazy' />
-                <img src={`${user3}`} alt="user3" className="Coins__image" loading='lazy' />
-                <img src={`${user4}`} alt="user4" className="Coins__image" loading='lazy' />
-                <img src={`${user5}`} alt="user5" className="Coins__image" loading='lazy' />
-                <small className='Coins__small'>Únete a nuestra comunidad de más de 2000 usuarios</small>
-        </div>
-    )
-}
+  return (
+    <div className="">
+      <div className="Coins__list">
+        <img
+          src={`${user1}`}
+          alt="user1"
+          className="Coins__image"
+          loading="lazy"
+        />
+        <img
+          src={`${user2}`}
+          alt="user2"
+          className="Coins__image"
+          loading="lazy"
+        />
+        <img
+          src={`${user3}`}
+          alt="user3"
+          className="Coins__image"
+          loading="lazy"
+        />
+        <img
+          src={`${user4}`}
+          alt="user4"
+          className="Coins__image"
+          loading="lazy"
+        />
+        <img
+          src={`${user5}`}
+          alt="user5"
+          className="Coins__image"
+          loading="lazy"
+        />
+        <small className="Coins__small Coins__small--pc">
+          Únete a nuestra comunidad de más de 2000 usuarios
+        </small>
+      </div>
+        <small className="Coins__small Coins__small--smartphone">
+          Únete a nuestra comunidad de más de 2000 usuarios
+        </small>
+    </div>
+  );
+};
