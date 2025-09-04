f2f36abbd554b642480c3ee4056ff8a3bdb1120d
“Your <you@example.com>
2025-09-04 19:49:50 +0200

    external links added  and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import "@/App.css";
 import { Header, Footer, Hero } from "@/components";
 import { Home } from "@/pages/Home/Home.jsx";
 import { PoliticaPrivadad } from "@/pages/PoliticaPrivadad/PoliticaPrivadad";
+import { Link, Route, Switch } from "wouter";
+import { useBrowserLocation } from "wouter/use-browser-location";
 function App() {
+  const [location] = useBrowserLocation();
   return (
     <>
       {/* 100% width */}
       <Header />
-      <Hero />
+      {location === "/falles360-Francesc" ? <Hero /> : null}
       {/* 80% width */}
       <div className="container">
         <div className="container__left-site">&nbsp;</div>
         <div className="container-center-site">
-          <Home />
-          <PoliticaPrivadad />
+          <Switch>
+            <Route path="/falles360-Francesc" component={Home} />
+            <Route
+              path="/falles360-Francesc/politica-privacidad"
+              component={PoliticaPrivadad}
+            />
+            <Route>404: Página no encontrada!</Route>
+          </Switch>
         </div>
       </div>
       {/* 100% width */}
