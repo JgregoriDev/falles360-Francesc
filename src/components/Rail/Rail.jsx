8e1a793cd6ca1ddd31038d9f958de94914a75850
“Your <you@example.com>
2025-09-10 20:57:29 +0200

    Overlay

--- src/components/Rail/Rail.jsx
+++ src/components/Rail/Rail.jsx
@@ -1,16 +1,50 @@
+import "./Rail.css";
+import Foto1 from "@/assets/fireworks.webp";
+import Foto2 from "@/assets/falla.webp";
+import Foto3 from "@/assets/lucesfallasValencia.webp";
 export const Rail = () => {
   return (
-    <div>
-      <h3> 1. Palmeras de fuegos artificiales</h3>
-      Recibe alertas al segundo, llega justo antes del estallido. No es solo una app.
-      Es tu radar fallero con ello no te perderas nada. Fallas 360. Donde los datos te llevan al momento
-      exacto.
-      <h3>2. Todo arde. Menos tu tiempo</h3>
-      Paga desde el móvil, entra con tu pulsera, salta colas sin tokens.
-Inscríbete, juega, gana. Que el show no espere por ti.. Fallas 360. Donde la magia también es digital.
-      <h3>3. Luces de noche típicas de Fallas</h3>
-      Mapa interactivo, acceso exclusivo, contenido solo para falleros.
-Comparte, comenta, conéctate a tu falla en un solo toque. Fallas 360. Luces, móvil... emoción.
+    <div className="container__rail">
+      <div className="">
+        <h3> 1. Palmeras de fuegos artificiales</h3>
+        <div className="container__overlay">
+          <img
+            className="foto"
+            src={Foto1}
+            loading="lazy"
+            alt="Palmeras de fuegos artificiales"
+          />
+          <p className="parraf">
+            Déjate llevar por la nostalgia y revive esos instantes mágicos. Con
+            nuestra Falles 360, cada evento se convierte en una experiencia
+            inolvidable, donde la tradición se encuentra con la modernidad.
+            ¡Vive las Fallas como nunca antes!
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <h3>2. Todo arde. Menos tu tiempo</h3>
+        <div className="container__overlay">
+          <img className="foto" loading="lazy" src={Foto2} alt="Falla circo" />
+
+          <p className="parraf">
+            Este año, deja que el fuego de la tradición ilumine tu camino,
+            mientras tú te sumerges en la esencia de lo que realmente importa:
+            los momentos que compartes y el tiempo que atesoras.
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <h3>3. Luces de noche típicas de Fallas</h3>
+        <div className="container__overlay">
+          <img className="foto" src={Foto3} loading="lazy" alt="" />
+          <p className="parraf">
+            Este año, deja que el fuego de la tradición ilumine tu camino,
+            mientras tú te sumerges en la esencia de lo que realmente importa:
+            los momentos que compartes y el tiempo que atesoras.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
