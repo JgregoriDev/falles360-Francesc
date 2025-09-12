58dccdce36e7c16c7eb6850bda0fcc8dfa8f7c82
“Your <you@example.com>
2025-09-12 20:10:41 +0200

    Faq list mejorado responsive

--- src/components/FAQList/FAQList.jsx
+++ src/components/FAQList/FAQList.jsx
@@ -4,6 +4,8 @@ export const FAQList = () => {
   if (FAQs === undefined || FAQs.length === 0) {
     return "No hay";
   }
+  // TODO: La flecha no sale a la misma altura que el texto corregir 
+  // TODO: Darle mas especio al texto de la respuesta y la pregunta
   return (
     <section id="preguntas" className="section faqs">
       <h2>Preguntas y respuestas sobre nuestros servicios</h2>
@@ -14,16 +16,16 @@ export const FAQList = () => {
             <label htmlFor={`radio${index}`} className="accordion__question">
               <h3 className="acordion__title">
                 {item.question}
+              </h3>
                 <svg
                   className="accordion__arrow"
-                  width={35}
-                  height={35}
+                  width={40}
+                  height={40}
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 640 640"
                 >
                   <path d="M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z" />
                 </svg>
-              </h3>
             </label>
             <p className="accordion__answer">{item.answer}</p>
           </div>
