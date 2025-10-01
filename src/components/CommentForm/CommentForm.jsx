20290a541655ed8d62fbec2124249bb00d384e43
“Your <you@example.com>
2025-10-01 18:24:15 +0200

    icono next , previous and  reponsive

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Tooltip from "@components/Tooltip/Tooltip.jsx";
 import "./CommentForm.css";
-import avatar from '@/assets/avatar.png';
+import avatar from "@/assets/avatar.png";
 const CommentForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -10,13 +10,26 @@ const CommentForm = () => {
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <label className="mb-1" htmlFor="message">Introduce tu comentario:   <Tooltip tooltipText={"Introduce tu comentario"} /></label>
-      <img src="" alt="" />
+      <label className="mb-1" htmlFor="message">
+        Introduce tu comentario:{" "}
+        <Tooltip tooltipText={"Introduce tu comentario"} />
+      </label>
       <div className="form__container">
-        <img src={avatar} title="avatar User 1" alt="" className="form__user--avatar" />
-        <textarea className="form__textArea" name="message" id="message" required></textarea>
-        </div>
-      
+        <img
+          loading="lazy"
+          src={avatar}
+          title="avatar User 1"
+          alt=""
+          className="form__user--avatar"
+        />
+        <textarea
+          className="form__textArea"
+          name="message"
+          id="message"
+          required
+        ></textarea>
+      </div>
+
       <button type="submit" className=" mb-2 mt-2 button button-primary">
         Enviar datos
       </button>
